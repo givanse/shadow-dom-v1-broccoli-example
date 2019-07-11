bac8667caee4f7aadc99921e11237cc1bfcc7f8a
Gastón I. Silva <givanse@gmail.com>
2019-07-10 17:21:32 -0700

    I forgot what is this

--- src/custom-element.ts
+++ src/custom-element.ts
@@ -1,7 +1,32 @@
 
+interface TemplatesHash {
+  [key: string]: Node;
+}
+
+const TEMPLATE_CACHE: TemplatesHash = {};
+
+function importTemplate(templateId: string): void {
+  const templatesLink = window.document.getElementById('ce-templates');
+
+  let templates;
+  if ('import' in templatesLink) { // HTML Imports, Chrome <= 80
+    templates = templatesLink.import;
+  }
+  const template = templates.querySelector('#' + templateId);
+  const deep = true;
+  const node = document.importNode(template.content, deep);
+  TEMPLATE_CACHE[templateId] = node;
+}
+
 export default class CustomElement extends HTMLElement {
 
   static tagName = 'custom-element';
+
+  // Specify observed attributes so that
+  // attributeChangedCallback will work
+  static get observedAttributes(): string[] {
+    return [];
+  }
 
   constructor() {
     super();
@@ -9,18 +34,35 @@ export default class CustomElement extends HTMLElement {
     this.addShadowRoot();
   }
 
-  addShadowRoot() {
+  connectedCallback() {}
+
+  disconnectedCallback() {}
+
+  adoptedCallback() {}
+
+  attributeChangedCallback(name: string, oldValue, newValue) {}
+
+  private addShadowRoot() {
     if (this.shadowRoot) {
       return;
     }
 
-    const templatesLink = window.document.getElementById('ce-templates');
-    const templates: HTMLDocument = templatesLink.import;
-    const templateId = this.constructor.tagName;
-    const template: HTMLTemplateElement = templates.querySelector('#' + templateId);
-
     const shadowRoot = this.attachShadow({mode: 'open'});
-    shadowRoot.appendChild(template.content.cloneNode(true));
+    const node = this.getTemplateNode();
+    shadowRoot.appendChild(node);
+  }
+
+  private getTemplateNode(): Node {
+    const templateId = this.constructor.tagName;
+
+    if (!TEMPLATE_CACHE[templateId]) {
+      importTemplate(templateId);
+    }
+
+    const node = TEMPLATE_CACHE[templateId];
+
+    const deep = true;
+    return node.cloneNode(deep);
   }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,21 @@ import LoremIpsum from './custom-elements/lorem-ipsum/index';
 import CustomElement from './custom-element';
 
 function registerCustomElement(CustomElement) {
-  if (window.customElements && window.customElements.define) {
+  if (false) {
+    // Chrome >= 80
+    // The proposal is to use ES modules, but it isn't a standard.
+  } else if (window.customElements && window.customElements.define) {
+    // Chrome <= 80
+    // Deprecated HTML Imports
+    // https://www.chromestatus.com/feature/5144752345317376
     window.customElements.define(CustomElement.tagName, CustomElement);
+  } else {
+    // Chrome <= 54
+    // Deprecated Custom Elements V0
+    // https://www.chromestatus.com/features/4642138092470272
+    document.registerElement(CustomElement.tagName, CustomElement/*{
+      prototype: Object.create(CustomElement.prototype)
+    }*/);
   }
 }
 
