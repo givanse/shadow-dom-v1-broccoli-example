911955b4de7143cfa4963eb3e043fa1e810db48a
Gastón I. Silva <givanse@gmail.com>
2019-07-15 07:42:01 -0700

    wip

--- Brocfile.ts
+++ Brocfile.ts
@@ -1,41 +1,39 @@
 import BroccoliCustomElements from "broccoli-custom-elements";
-import rollup from "broccoli-rollup";
 const mergeTrees = require("broccoli-merge-trees");
-const babel = require("rollup-plugin-babel");
-const resolve = require("rollup-plugin-node-resolve");
 const funnel = require("broccoli-funnel");
-const commonjs = require("rollup-plugin-commonjs");
+//const babel = require("rollup-plugin-babel");
+//const resolve = require("rollup-plugin-node-resolve");
+//const commonjs = require("rollup-plugin-commonjs");
 
 export default function(/*options*/) {
 
   const staticFiles = funnel("src", {
     include: [
       "index.html"
-    ]
+    ],
+    annotation: "Static Files"
   });
 
+  //const extensions = [".js", ".ts"];
+
+  /*const rollupConfig = {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "bundle.iife.js",
+        name: "customElements",
+        format: "iife",
+        sourcemap: false,
+      },
+    ],
+    plugins: [
+      commonjs(),
+      resolve({extensions}),
+      babel({extensions})
+    ],
+  };*/
+
   const customElements = new BroccoliCustomElements("src/custom-elements");
-
-  const extensions = [".js", ".ts"];
-
-  /*const js = rollup(".", {
-    rollup: {
-      input: "src/index.ts",
-      output: [
-        {
-          file: "bundle.iife.js",
-          name: "customElements",
-          format: "iife",
-          sourcemap: false,
-        },
-      ],
-      plugins: [
-        commonjs(),
-        resolve({extensions}),
-        babel({extensions})
-      ],
-    }
-  });*/
 
   return mergeTrees([staticFiles, customElements]);
 }
